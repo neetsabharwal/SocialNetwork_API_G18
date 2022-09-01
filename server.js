4260b685d52e56ae789ba28bf9efff2fb101f560
Gurneet <gsingh2@atb.com>
2022-09-01 19:53:41 -0400

    started code loaded

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-//cmment
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
